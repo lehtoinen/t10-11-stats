8cb2ea433625c4a521dd3125c9e14dec4ab33c3d
lehtoinen <lehtovaaralainen@gmail.com>
2023-04-13 21:24:15 +0300

    Add react and github pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://lehtoinen.github.io',
+  base: '/t10-11-stats',
+  integrations: [react()]
+});
